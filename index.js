5ac0f9dbfcdfd962d45bd1368109a13986ab567f
Roman Isko <iskoroman@gmail.com>
2018-01-02 14:19:15 +0200

    implemented sending country capital and flag

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Telegraf = require('telegraf');
-const rp       = require('request-promise');
-const bot      = new Telegraf(process.env.BOT_TOKEN);
-const Markup   = require('telegraf/markup');
+const Telegraf  = require('telegraf');
+const rp        = require('request-promise');
+const bot       = new Telegraf(process.env.BOT_TOKEN);
+const Markup    = require('telegraf/markup');
+const {convert} = require('convert-svg-to-png');
 
 bot.telegram.getMe().then((botInfo) => {
     bot.options.username = botInfo.username
@@ -19,13 +20,13 @@ bot.hears(/\/s (.+)/, ({match, reply}) => {
     }
     rp({
         method: 'GET',
-        uri   : 'https://restcountries.eu/rest/v2/name/' + match[1] + '?fields=name',
+        uri   : 'https://restcountries.eu/rest/v2/name/' + match[1] + '?fields=name;alpha3Code',
         json  : true
     })
         .then(resp => {
             let keyboard = [];
             for (let country of resp) {
-                keyboard.push(Markup.callbackButton(country.name, `c:${country.name}`));
+                keyboard.push(Markup.callbackButton(country.name, `c:${country.alpha3Code}`));
             }
             reply('Please choose a country:',
                 Markup.inlineKeyboard(keyboard, {columns: 2}).extra()
@@ -35,8 +36,22 @@ bot.hears(/\/s (.+)/, ({match, reply}) => {
 });
 
 bot.action(/c:.+/, (ctx) => {
-    let country = ctx.match[0].replace('c:', '');
-    return ctx.reply(`Oh, ${country}! Great choice`)
+    let countryCode = ctx.match[0].replace('c:', '');
+    rp({
+        method: 'GET',
+        uri   : `https://restcountries.eu/rest/v2/alpha/${countryCode}`,
+        json  : true
+    })
+        .then(resp => {
+            ctx.reply(resp.capital);
+            rp(resp.flag)
+                .then(svgResp =>
+                    convert(svgResp)
+                        .then(png =>
+                            ctx.replyWithPhoto({source: png})
+                        ))
+        })
+        .catch(err => console.log(err));
 });
 
 bot.startPolling();
