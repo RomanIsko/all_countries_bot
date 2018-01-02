a112690cb294ec8ae6f7ee16a3cb1dda4d6d986e
Roman Isko <iskoroman@gmail.com>
2018-01-02 13:28:25 +0200

    implemented keyboard for search results

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf');
 const rp       = require('request-promise');
 const bot      = new Telegraf(process.env.BOT_TOKEN);
+const Markup   = require('telegraf/markup');
 
 bot.telegram.getMe().then((botInfo) => {
     bot.options.username = botInfo.username
@@ -12,7 +13,7 @@ bot.start((ctx) => {
 });
 
 bot.command('help', (ctx) => ctx.reply('TODO: here will be help message'));
-bot.hears(/s (.+)/ || /search (.+)/, ({match, reply}) => {
+bot.hears(/\/s (.+)/, ({match, reply}) => {
     if (match[1].length < 2) {
         return reply('Please provide at least 2 chars')
     }
@@ -22,11 +23,20 @@ bot.hears(/s (.+)/ || /search (.+)/, ({match, reply}) => {
         json  : true
     })
         .then(resp => {
+            let keyboard = [];
             for (let country of resp) {
-                reply(country.name)
+                keyboard.push(Markup.callbackButton(country.name, `c:${country.name}`));
             }
+            reply('Please choose a country:',
+                Markup.inlineKeyboard(keyboard, {columns: 2}).extra()
+            )
         })
         .catch(err => console.log(err))
+});
+
+bot.action(/c:.+/, (ctx) => {
+    let country = ctx.match[0].replace('c:', '');
+    return ctx.reply(`Oh, ${country}! Great choice`)
 });
 
 bot.startPolling();
